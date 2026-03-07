610806b32b943caf2c02a8ef51a1eed14f2d8ef1
AmitDinnimani <amitdinnimani@gmail.com>
2026-03-07 22:05:55 +0530

    some updates

--- scripts/serve_model.py
+++ scripts/serve_model.py
@@ -1,4 +1,5 @@
 import mlflow.pyfunc
+import uvicorn
 
 mlflow.set_tracking_uri("http://mlflow:5000")
 
@@ -7,3 +8,7 @@ def load_model_for_prediction():
     model = mlflow.pyfunc.load_model("models:/churn_prediction_model/Production")
 
     return model
+
+
+if __name__ == "__main__":
+    uvicorn.run("src.api.main:app", host="0.0.0.0", port=8000)
